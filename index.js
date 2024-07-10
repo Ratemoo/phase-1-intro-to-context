a7828f14154bd9f2c05a837d5cc78f4608a9ae85
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-07-10 13:19:06 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // Your code here
+// index.js
+
+// Helper functions
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employee, dateTimeString) {
+    let [date, hour] = dateTimeString.split(' ');
+  
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+  
+    return employee;
+  }
+  
+  function createTimeOutEvent(employee, dateTimeString) {
+    let [date, hour] = dateTimeString.split(' ');
+  
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+  
+    return employee;
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    let timeInEvent = employee.timeInEvents.find(event => event.date === date);
+    let timeOutEvent = employee.timeOutEvents.find(event => event.date === date);
+  
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    let hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  function allWagesFor(employee) {
+    let datesWorked = employee.timeInEvents.map(event => event.date);
+  
+    let totalWages = datesWorked.reduce((total, date) => {
+      return total + wagesEarnedOnDate(employee, date);
+    }, 0);
+  
+    return totalWages;
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => {
+      return totalPayroll + allWagesFor(employee);
+    }, 0);
+  }
+  
+  
+  
